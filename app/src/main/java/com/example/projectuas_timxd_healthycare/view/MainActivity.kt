1be9aa4b773687262b089c2bdaf213f512bfd594
andreasn18 <tandiono4@gmail.com>
2021-05-21 16:07:08 +0700

    UI FoodLog

--- app/src/main/java/com/example/projectuas_timxd_healthycare/view/MainActivity.kt
+++ app/src/main/java/com/example/projectuas_timxd_healthycare/view/MainActivity.kt
@@ -6,17 +6,21 @@ import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.example.projectuas_timxd_healthycare.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var navController: NavController
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        navController = Navigation.findNavController(this,R.id.fragmentHost)
-//        NavigationUI.setupActionBarWithNavController(this, navController)
+        setSupportActionBar(findViewById(R.id.toolbar))
+        navController = Navigation.findNavController(this, R.id.fragmentHost)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navView, navController)
     }
-//    override fun onSupportNavigateUp(): Boolean {
-//        return NavigationUI.navigateUp(navController,null)
-//    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout)
+    }
 }
